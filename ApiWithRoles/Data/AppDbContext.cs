e671d5f6a5af153d0a8901428e891eb8e0ddb463
amdadulhaquehasan <amdadul.hasan75757@gmail.com>
2025-10-05 15:47:11 +0600

    Add AppDbContext File

---
+++ ApiWithRoles/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiWithRoles.Data;
+
+public class AppDbContext : IdentityDbContext<IdentityUser>
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
